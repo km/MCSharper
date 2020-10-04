6e9e37977cef381066126d1ca701916fda4288d8
enderomar <39051822+enderomar@users.noreply.github.com>
2020-10-04 07:54:09 +0300

    Add files via upload

---
+++ MCSharper/MCApi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCSharper
+{
+    class MCApi
+    {
+        public string mainendpoint = "api.mojang.com";
+    }
+}
