40449ffafd5fa0b9c6ddde485ec51d7fadffa854
Oval <42103580+Oval@users.noreply.github.com>
2020-10-10 21:11:23 +0200

    Update and rename MCApiToken.cs to MCAPIToken.cs
    Clean up of code and naming to stop confusion

---
+++ MCSharper/MCAPIToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCSharper
+{
+    class MCAPIToken
+    {
+        public AuthedEndpoint authedEndpoint;
+        
+        public MCAPIToken(AuthedEndpoint authEndpoint)
+        {
+            authedEndpoint = authEndpoint;
+        }
+    }
+}
