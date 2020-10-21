9fb27f37907b45c3ed1205fbdb63330471ae46f9
enderomar <39051822+enderomar@users.noreply.github.com>
2020-10-21 03:31:56 +0300

    New TokenProperties and Proxy Support
    -TokenProperties class
    -HTTPS proxy support
    -Client token is now optional for authedendpoint or any of its inherited classes

--- MCSharper/NameProperties.cs
+++ MCSharper/NameProperties.cs
@@ -10,47 +10,63 @@ namespace MCSharper
     {
         public string name;
         public string UUID;
-        public bool taken;
         public NameProperties(string Name) : base(null)
         {
             name = Name;
         }
         //returns all uuids sorted from oldest to latest with optional paramter of capping the uuids (use 1 for the latest uuid)
-        public List<string> grabUUID(int amount = 1)
+        public List<string> grabAllUUID(int amount = Int32.MaxValue)
         {
-            method = "GET";
-            url = "https://api.mojang.com/";
+
             List<string> uuidlist = new List<string>();
             long currentepoch = DateTimeOffset.Now.ToUnixTimeSeconds();
             string uuid = "";
             using (wc)
             {
+                url = "https://api.mojang.com/";
+                method = "GET";
                 while (currentepoch > 1420154260)
                 {
-                    if (uuidlist.Count >= amount) { break; }
+                    if (uuidlist.Count >= amount)
+                    {
+                        break;
+                    }
+
                     string Suuid = uuid;
-                    string payload = string.Format(url+ "users/profiles/minecraft/{0}?at=" + currentepoch, name);
-                    string response = wc.DownloadString(payload);
+
                     try
                     {
+                        string payload = string.Format(url + "users/profiles/minecraft/{0}?at=" + currentepoch, name);
+                        string response = wc.DownloadString(payload);
+
                         uuid = JObject.Parse(response)["id"].ToString();
                     }
-                    catch { }
-                    currentepoch -= (long)3196800;
-                    if (uuid != Suuid) { uuidlist.Add(uuid); }
+                    catch
+                    {
+                    }
+
+                    currentepoch -= (long) 3196800;
+                    if (uuid != Suuid)
+                    {
+                        uuidlist.Add(uuid);
+                    }
                 }
 
             }
+
             UUID = uuidlist[0]; //sets latest uuid to the classes uuid
             uuidlist.Reverse();
             return uuidlist;
         }
+
+        //Grabs uuid at the timestamp
         public string UUIDAtTimeStamp(long UnixSeconds) 
         {
-            method = "GET";
-            url = "https://api.mojang.com/";
+   
             using (wc)
             {
+                url = "https://api.mojang.com/";
+                method = "GET";
                 string payload = string.Format(url + "users/profiles/minecraft/{0}?at=" + UnixSeconds, name);
                 string response = wc.DownloadString(payload);
                 try
@@ -62,13 +78,15 @@ namespace MCSharper
 
             return UUID;
         }
+        //Grabs uuid at the timestamp
         public string UUIDAtTimeStamp(DateTimeOffset time)
         {
-            method = "GET";
-            url = "https://api.mojang.com/";
+          
             long unix = time.ToUnixTimeSeconds();
             using (wc) 
             {
+                url = "https://api.mojang.com/";
+                method = "GET";
                 string payload = string.Format(url + "users/profiles/minecraft/{0}?at=" + unix, name);
                 string response = wc.DownloadString(payload);
                 try
@@ -83,11 +101,13 @@ namespace MCSharper
         //Checks if the name is available
         public bool isAvailable() 
         {
-            url = "https://account.mojang.com/available/minecraft/";
-            method = "GET";
+         
             using (wc) 
             {
+                url = "https://account.mojang.com/available/minecraft/";
+                method = "GET";
                 string response = wc.DownloadString(url + name);
+                Console.WriteLine(response);
                 if (response == "TAKEN") { return false; }
                 else { return true; }
             }
@@ -95,10 +115,10 @@ namespace MCSharper
         //Returns if the name is blocked whether by a user temporarily or by mojang permanently (requires token)
         public bool isBlocked() 
         {
-            url = "https://api.mojang.com/user/profile/agent/minecraft/name/";
-            method = "GET";
             using (wc)
             {
+                url = "https://api.mojang.com/user/profile/agent/minecraft/name/";
+                method = "GET";
                 try
                 {
                     wc.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + Token);
@@ -108,6 +128,30 @@ namespace MCSharper
                 }
                 catch { return false; }
             }
+        }
+        //gets the droptime of an upcoming name will
+        public long dropTime()
+        {
+            long drop = 0;
+            using (wc)
+            {
+
+                    UUID = UUIDAtTimeStamp(DateTimeOffset.Now.ToUnixTimeSeconds()- 3196800);
+                    method = "GET";
+                    url = "https://api.mojang.com/user/profiles/";
+                    string p = url + UUID + "/names";
+                    Console.WriteLine(p);
+                    string list = wc.DownloadString(p);
+                    var jr = JArray.Parse(list);
+                    List<JToken> jlist = new List<JToken>();
+                    foreach (var uwu in jr)
+                    {
+                        if (uwu["name"].ToString().ToUpper() == name.ToUpper()) { jlist.Add(uwu); }
+                    }
+                    drop = Convert.ToInt64(jr[jr.IndexOf(jlist[jlist.Count - 1]) + 1]["changedToAt"]) + 3196800000;
+             
+            }
+            return drop;
         }
     }
 }
